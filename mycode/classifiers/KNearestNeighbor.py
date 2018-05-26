5da71ba6cb352935d12890c87f6c952fbb07fb4b
bartek <kicjow.ketrab@gmail.com>
2018-05-26 14:35:26 +0100

    init

---
+++ mycode/classifiers/KNearestNeighbor.py
@@ -0,0 +1,122 @@
+import numpy as np
+
+
+class KNearestNeighbor(object):
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        """
+        Train the classifier. For k-nearest neighbors this is just
+        memorizing the training data.
+
+        Inputs:
+        - X: A numpy array of shape (num_train, D) containing the training data
+          consisting of num_train samples each of dimension D.
+        - y: A numpy array of shape (N,) containing the training labels, where
+             y[i] is the label for X[i].
+        """
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X, k=1, num_loops=0):
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dists = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError('Invalid value %d for num_loops' % num_loops)
+
+        return self.predict_labels(dists, k=k)
+
+    def compute_distances_two_loops(self,X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test,num_train))
+        for i in range(num_test):
+            for j in range(num_train):
+                #####################################################################
+                # TODO:                                                             #
+                # Compute the l2 distance between the ith test point and the jth    #
+                # training point, and store the result in dists[i, j]. You should   #
+                # not use a loop over dimension.                                    #
+                #####################################################################
+                pass
+                debug = 5
+                t1 = X[i]
+                t2 = self.X_train[j]
+                thisDist = np.sum(np.abs(t1-t2))
+                dists[i,j] = thisDist
+                #####################################################################
+                #                       END OF YOUR CODE                            #
+                #####################################################################
+        return dists
+
+
+    def compute_distances_no_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        pass
+        return dists
+
+    def predict_labels(self, dists, k=1):
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+        for i in range(num_test):
+            # A list of length k storing the labels of the k nearest neighbors to
+            # the ith test point.
+            closest_y = []
+            #########################################################################
+            # TODO:                                                                 #
+            # Use the distance matrix to find the k nearest neighbors of the ith    #
+            # testing point, and use self.y_train to find the labels of these       #
+            # neighbors. Store these labels in closest_y.                           #
+            # Hint: Look up the function numpy.argsort.                             #
+            #########################################################################
+            pass
+            row = dists[i]
+            min_indexes = np.argsort(row)[:k]
+            min_indexes = min_indexes.reshape(1,k)
+            labels = []
+            for id, min_idx in enumerate(min_indexes):
+                labels.append(self.y_train[min_idx])
+            #########################################################################
+            # TODO:                                                                 #
+            # Now that you have found the labels of the k nearest neighbors, you    #
+            # need to find the most common label in the list closest_y of labels.   #
+            # Store this label in y_pred[i]. Break ties by choosing the smaller     #
+            # label.                                                                #
+            #########################################################################
+            pass
+            y_pred[i] = np.argmax(np.bincount(np.array(labels)[0]))
+            debug = 5
+            #########################################################################
+            #                           END OF YOUR CODE                            #
+            #########################################################################
+
+        return y_pred
+
+    def compute_distances_one_loop(self, X):
+        """
+        Compute the distance between each test point in X and each training point
+        in self.X_train using a single loop over the test data.
+
+        Input / Output: Same as compute_distances_two_loops
+        """
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            #######################################################################
+            # TODO:                                                               #
+            # Compute the l2 distance between the ith test point and all training #
+            # points, and store the result in dists[i, :].                        #
+            #######################################################################
+            pass
+            #######################################################################
+            #                         END OF YOUR CODE                            #
+            #######################################################################
+        return dists
---
+++ mycode/knnLogic.py
@@ -0,0 +1,84 @@
+import random
+import numpy as np
+from myDataUtils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+cifar10_dir = '../cs231n/datasets/cifar-10-batches-py'
+
+try:
+   del X_train, y_train
+   del X_test, y_test
+   print('Clear previously loaded data.')
+except:
+   pass
+
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+# As a sanity check, we print out the size of the training and test data.
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+num_classes = len(classes)
+samples_per_class = 7
+
+# for y, cls in enumerate(classes):
+#     idxs = np.flatnonzero(y_train == y)
+#     idxs = np.random.choice(idxs, samples_per_class, replace=False)
+#     for i, idx in enumerate(idxs):
+#         plt_idx = i * num_classes + y + 1
+#         plt.subplot(samples_per_class, num_classes, plt_idx)
+#         plt.imshow(X_train[idx].astype('uint8'))
+#         plt.axis('off')
+#         if i == 0:
+#             plt.title(cls)
+#
+# plt.ioff()
+# plt.show()
+
+# Subsample the data for more efficient code execution in this exercise
+num_training = 5000
+mask = list(range(num_training))
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+num_test = 500
+mask = list(range(num_test))
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+print(X_train.shape, X_test.shape)
+
+from classifiers.KNearestNeighbor import KNearestNeighbor
+
+classifier = KNearestNeighbor()
+classifier.train(X_train, y_train)
+
+dists = classifier.compute_distances_two_loops(X_test)
+print(dists.shape)
+
+# We can visualize the distance matrix: each row is a single test example and
+# its distances to training examples
+plt.imshow(dists, interpolation='none')
+plt.ioff()
+# plt.show()
+debug = 5
+
+# Now implement the function predict_labels and run the code below:
+# We use k = 1 (which is Nearest Neighbor).
+y_test_pred = classifier.predict_labels(dists, k=1)
+
+# Compute and print the fraction of correctly predicted examples'numpy.int64' object is not iterable
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+
